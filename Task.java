c2add60f59398f107ff5b0620c8f6469cf343481
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-05-01 10:37:11 +0000

    [追記]all contents

--- Task.java
+++ Task.java
@@ -20,5 +20,13 @@
     System.out.println("体重は" + weight + "kgです");
     System.out.println("BMIは" + bmi + "です");
 
+     // 名前、年齢、身長、体重、BMIに関する情報を出力してください
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
+
+
   }
 }
